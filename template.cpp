992ef704374a8abc549c952a8265b6057b7c3b52
Pawan Nirpal <49726539+pawan-nirpal-031@users.noreply.github.com>
2019-11-29 11:46:32 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -15,9 +15,12 @@ using namespace std;
 typedef unsigned long long int ull;
 typedef long double ld;
 #define MOD 1000000007;
-#define pb(a) push_back(a);
-#define mp(a,b) make_pair(a,b)
-#define _for(k,v) for(auto x : v){ cout<<x<<'\n'; }
+#define pub(a) push_back(a);
+#define mp(a,b) make_pair(a,b);
+#define aout(x,v) for(auto x : v){cout<<x<<'\n';} // array output function only
+#define ain(a,n) for(ull i =0;i<n;i++){cin>>a[i];} //array input function
+#define getl(s) getline(cin,s);
+#define in(x) cin>>x;
 ull Fast_Exp(ull a,ull n) // O(log(n)) time 
 {
     ull x =0;
@@ -43,6 +46,7 @@ ull Fast_Exp(ull a,ull n) // O(log(n)) time
     }
     
 }
+
 ull binpow(ull a,ull b)//binary exponentaion (O(log(d))) where d is no of bits in base 2 represenation of power
 {
    ull res = 1;
@@ -57,6 +61,7 @@ ull binpow(ull a,ull b)//binary exponentaion (O(log(d))) where d is no of bits i
    }
    return res;
 }
+
 ull Mod_exp(ull a, ull b, ull m)//function to compute (a power b)mod(m)
 {
     a %= m;
@@ -71,23 +76,13 @@ ull Mod_exp(ull a, ull b, ull m)//function to compute (a power b)mod(m)
     }
     return res;
 }
+
 ull gcd_euclid(ull a, ull b)// Important : [ make sure function call for this function is made like this : gcd_euclid( min(a,b) , max(a,b) ) ]
-{
-    if (b%a==0)
-    {
-        return a;
-    }
-    else
-    {
-        return gcd_euclid(b%a,a);
-    }
-    
-    
-}
+{ if(b%a==0){return a;}else{return gcd_euclid(b%a,a);} }
+
 inline ull lcm(ull a,ull b)
-{
-    return (a*b)/(gcd_euclid(min(a,b),max(a,b)));
-}
+{ return (a*b)/(gcd_euclid(min(a,b),max(a,b))); }
+
 bool is_Prime(ull x)
 {
     bool yes = true;
@@ -112,9 +107,11 @@ bool is_Prime(ull x)
    
     
 }
+
 int main()
 {
   ios_base::sync_with_stdio(false);
+  
   
   
    return 0;
